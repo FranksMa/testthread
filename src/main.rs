243ca4fbdd0b45df3ecb8c2c4bbc8b1a992c04f3
matianqi <mtqmx3@gmail.com>
2021-01-03 16:33:59 +0800

    json 序列化 serde 使用

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,9 @@
-struct Duck;
-struct Pig;
-
-// 特征
-trait Fly {
-    // 具备该特征的行为
-    fn fly(&self) -> bool;
-    fn is_wings(&self);
-}
-
-impl Fly for Duck {
-    fn fly(&self) -> bool {
-        return true;
-    }
-
-    fn is_wings(&self) {
-        println!("I have a wing");
-    }
-}
-
-impl Fly for Pig {
-    fn fly(&self) -> bool {
-        return false;
-    }
-
-    fn is_wings(&self) {
-        println!("I don't have a wing");
-    }
-}
-
-// 静态分发
-fn fly_static<T: Fly>(s: T) -> bool {
-    s.fly()
-}
-
-// 动态分发
-fn fly_dyn(s: &dyn Fly) -> bool {
-    s.fly()
-}
+extern crate serde;
 
 fn main() {
-    let pig = Pig;
-    pig.is_wings();
-    assert_eq!(fly_static::<Pig>(pig), false);
-
-    let duck = Duck;
-    duck.is_wings();
-    assert_eq!(fly_static::<Duck>(duck), true);
-    assert_eq!(fly_dyn(&Pig), false);
-    assert_eq!(fly_dyn(&Duck), true);
+    let x: i32 = 5;
+    let xs = serde_json::to_string(&x).unwrap();
+    println!("i32 number {} serializes into string {}", x, xs);
+    let xd: i32 = serde_json::from_str(&xs).unwrap();
+    assert_eq!(x, xd);
 }
