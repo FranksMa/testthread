01c351186d0518356ef0a6b181750cf07b31c483
matianqi <mtqmx3@gmail.com>
2021-01-03 13:21:30 +0800

    trait test

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,13 @@ struct Duck;
 struct Pig;
 
 // 特征
-trait Fly{
+trait Fly {
     // 具备该特征的行为
     fn fly(&self) -> bool;
     fn is_wings(&self);
 }
-impl Fly for Duck {
 
+impl Fly for Duck {
     fn fly(&self) -> bool {
         return true;
     }
@@ -16,11 +16,9 @@ impl Fly for Duck {
     fn is_wings(&self) {
         println!("I have a wing");
     }
-
 }
 
 impl Fly for Pig {
-
     fn fly(&self) -> bool {
         return false;
     }
@@ -28,13 +26,13 @@ impl Fly for Pig {
     fn is_wings(&self) {
         println!("I don't have a wing");
     }
-
 }
 
 // 静态分发
 fn fly_static<T: Fly>(s: T) -> bool {
     s.fly()
 }
+
 // 动态分发
 fn fly_dyn(s: &dyn Fly) -> bool {
     s.fly()
@@ -42,10 +40,12 @@ fn fly_dyn(s: &dyn Fly) -> bool {
 
 fn main() {
     let pig = Pig;
-    assert_eq!(fly_static::<Pig>(pig),false);
+    pig.is_wings();
+    assert_eq!(fly_static::<Pig>(pig), false);
 
     let duck = Duck;
-    assert_eq!(fly_static::<Duck>(duck),true);
-    assert_eq!(fly_dyn(&Pig),false);
-    assert_eq!(fly_dyn(&Duck),true);
+    duck.is_wings();
+    assert_eq!(fly_static::<Duck>(duck), true);
+    assert_eq!(fly_dyn(&Pig), false);
+    assert_eq!(fly_dyn(&Duck), true);
 }
