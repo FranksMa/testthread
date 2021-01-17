96a5ad0c1b307b88b84b3ffd71a1b84e41ca3b80
matianqi <mtqmx3@gmail.com>
2021-01-17 10:37:00 +0800

    对象

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,57 @@
-extern crate serde;
+struct Circle {
+    x: f64,
+    y: f64,
+    radius: f64,
+}
+
+struct CircleBuilder {
+    x: f64,
+    y: f64,
+    radius: f64,
+}
+
+impl Circle {
+    fn area(&self) -> f64 {
+        std::f64::consts::PI * (self.radius * self.radius)
+    }
+
+    fn new() -> CircleBuilder{
+        CircleBuilder {
+            x: 0.0,
+            y: 0.0,
+            radius: 1.0,
+        }
+    }
+}
+
+impl CircleBuilder {
+    fn x(&mut self, coordinates: f64) -> &mut CircleBuilder {
+        self.x = coordinates;
+        self
+    }
+
+    fn y(&mut self, coordinates: f64) -> &mut CircleBuilder {
+        self.y = coordinates;
+        self
+    }
+
+    fn radius(&mut self, radius: f64) -> &mut CircleBuilder {
+        self.radius = radius;
+        self
+    }
+
+    fn build(&self) -> Circle {
+        Circle {
+            x: self.x,
+            y: self.y,
+            radius: self.radius,
+        }
+    }
+}
 
 fn main() {
-    let x: i32 = 5;
-    let xs = serde_json::to_string(&x).unwrap();
-    println!("i32 number {} serializes into string {}", x, xs);
-    let xd: i32 = serde_json::from_str(&xs).unwrap();
-    assert_eq!(x, xd);
+    let c = Circle::new().x(1.0).y(2.0).radius(2.0).build();
+    assert_eq!(c.area(),12.0);
+    assert_eq!(c.x, 1.0);
+    assert_eq!(c.y, 2.0);
 }
