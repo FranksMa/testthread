0040b48c95125f95ef3adae697fa00c48b870286
matianqi <mtqmx3@gmail.com>
2021-01-17 14:40:36 +0800

    thread

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,14 @@
 use std::thread;
-use std::time::Duration;
+use std::sync::mpsc;
 
 fn main() {
-    let handle = thread::spawn(|| {
-        for i in 0..5 {
-            println!("spawned thread print {}", i);
-            thread::sleep(Duration::from_millis(1));
-        }
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let val = String::from("hi");
+        tx.send(val).unwrap();
     });
 
-    for i in 0..3 {
-        println!("main thread print {}", i);
-        thread::sleep(Duration::from_millis(1));
-    }
-    // 强制子线程结束后再结束主线程
-    handle.join().unwrap();
+    let received = rx.recv().unwrap();
+    println!("Got: {}", received);
 }
