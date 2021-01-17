e297bd0cab9aa338256dd94755351b50bcec07a4
matianqi <mtqmx3@gmail.com>
2021-01-17 21:03:03 +0800

    thread

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,79 @@
-use std::thread;
-use std::sync::mpsc;
+pub trait Summary {
+    fn summarize(&self) -> String {
+        String::from("(read more...)")
+    }
+}
+
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summary for NewsArticle {
+    fn summarize(&self) -> String {
+        format!("{}, by {} ({})", self.headline, self.author, self.location)
+    }
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Tweet {
+    fn summarize(&self) -> String {
+        format!("{}: {}", self.username, self.content)
+    }
+}
+
+pub struct NewComment {
+    pub author: String,
+}
+
+impl Summary for NewComment {}
+
+// 使用 trait 作为参数，参数对象必须有 trait 的实现
+// 我们可以传递任何 NewsArticle 或 Tweet 的实例来调用 notify。
+// 任何用其它如 String 或 i32 的类型调用该函数的代码都不能编译，因为它们没有实现 Summary。
+
+pub fn notify(item: impl Summary) {
+    println!("Breaking news {}",item.summarize());
+}
 
 fn main() {
-    let (tx, rx) = mpsc::channel();
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    };
 
-    thread::spawn(move || {
-        let val = String::from("hi");
-        tx.send(val).unwrap();
-    });
+    let article = NewsArticle {
+        headline: String::from("hello world"),
+        location: String::from("beijing"),
+        author: String::from("matianqi"),
+        content: String::from("read the book")
+    };
 
-    let received = rx.recv().unwrap();
-    println!("Got: {}", received);
+    let comment = NewComment {
+        author: String::from("matianqi")
+    };
+    
+    println!("1 new tweet: {}", tweet.summarize());
+
+    println!("1 new article: {}",article.summarize());
+
+    println!("1 new comment: {}", comment.summarize());
+    
+    notify(comment);
+
+    notify(article);
+
+    notify(tweet);
+
+    
 }
